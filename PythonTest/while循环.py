01cf68dc2388e3cd8dcb0611f19b9f0f3c38847b
ALcaWu <wwjlunar@gmail.com>
2017-11-09 21:28:22 +0800

    third

--- PythonTest/while循环.py
+++ PythonTest/while循环.py
@@ -30,13 +30,3 @@ while True:
     else:
         print("I'd love to go to " + city.title() + "!")
 
-# even_or_odd
-number = input("Enter a number, and I'll tell you if it's even or odd: ")
-number = int(number)
-if number % 2 == 0:
-    print("\nThe number " + str(number) + " is even.")
-else:
-    print("\nThe number " + str(number) + " is odd.")
-# test 7-1
-message = input("你想要租赁什么样的汽车： ")
-print("让我帮你找找看有没有" + message)
