d0d5cb61b436c242784c33142d05965d2abf05fe
ALca <djlunar@126.com>
2018-11-11 20:16:55 +0800

    第三日练习。

--- PythonTest/greeter.py
+++ PythonTest/greeter.py
@@ -4,3 +4,19 @@ def greet_user(username):
 
 
 greet_user('Wu')
+
+
+# 练习
+def display_message():
+    print("我学习了函数")
+
+
+display_message()
+
+
+# 练习2
+def favorite_book(bookname):
+    print("One of my favorite books is " + bookname.title())
+
+
+favorite_book('Dark Tower')
